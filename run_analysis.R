bdbb66c24931bbc76a4d875edd129b0b7e42f7d2
chemme <clhgit@gmail.com>
2014-09-21 08:04:53 -0500

    modification to improve data frame names

--- run_analysis.R
+++ run_analysis.R
@@ -20,12 +20,13 @@ PrepareData <- function () {
 
   subject <- c(subject_train$V1, subject_test$V1) # merge subject data
   y <- sapply(c(y_train$V1, y_test$V1), function(x) act_labels[x,2]) # replace numbers with labels
-  labels <- data.frame(subject = subject, y = y) # row labels
+  labels <- data.frame(subject = subject, activity = y) # row labels
   sub_merged <- subset( # subset labeled mean/std data
     merged,
     select = grep("mean\\(|std\\(", names(merged), value=TRUE) # extract mean/std headers
   ) 
   sub_merged <- cbind(labels, sub_merged) # merge activity labels with subset data and return
+  sub_merged$subject <- as.factor(sub_merged$subject) # convert subject column to factor
   sub_merged
 }
 
